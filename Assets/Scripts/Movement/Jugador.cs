2aaee54980f14e902b84a632a3fbb6b218b72906
SebasOso <83346917+SebasOso@users.noreply.github.com>
2023-03-04 16:21:14 -0500

    Jump

--- Assets/Scripts/Movement/Jugador.cs
+++ Assets/Scripts/Movement/Jugador.cs
@@ -6,22 +6,32 @@ using UnityEngine.Events;
 public class Jugador : MonoBehaviour
 {
     public float jumpForce = 10f;
-    public Rigidbody rb;
+    public float doubleJumpForce = 5f;
+    public float jumpDelay = 0.25f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public Transform groundCheck;
     public float speed = 5f;
     public float rotationSpeed = 50f;
+    public Rigidbody rb;
+
+
+    private bool isGrounded = true;
+    private bool canDoubleJump = false;
+    private float jumpDelayTimer;
+
+
+
     public SpawnEnemy spawnEnemy;
     public UnityEvent OnPlayerMove;
     private bool enemySpawn;
     // Update is called once per frame
     void Update()
     {
+        JumpSystem();
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
@@ -43,6 +53,37 @@ public class Jugador : MonoBehaviour
         {
             spawnEnemy.Spawn();
             enemySpawn = true;
+        }
+    }
+
+    private void Jump(float jumpForce)
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+    private void JumpSystem()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                Jump(jumpForce);
+                canDoubleJump = true;
+                jumpDelayTimer = jumpDelay;
+            }
+            else if (canDoubleJump)
+            {
+                Jump(doubleJumpForce);
+                canDoubleJump = false;
+            }
+        }
+        if (jumpDelayTimer > 0f && !isGrounded)
+        {
+            jumpDelayTimer -= Time.deltaTime;
         }
     }
 }
